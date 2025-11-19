1005687f38b9aa30e83eea28943befb15a75529c
Brt <barthunerbeincoding@gmail.com>
2025-11-19 16:20:19 +0100

    refactor tomatoes.js and vampires.js for improved code structure and readability

--- tomatoes.js
+++ tomatoes.js
@@ -1,62 +1,64 @@
 const exampleData = {
-			"dt": 1763564400,
-			"main": {
-				"temp": 277.84,
-				"feels_like": 274.33,
-				"temp_min": 277.84,
-				"temp_max": 277.98,
-				"pressure": 1002,
-				"sea_level": 1002,
-				"grnd_level": 993,
-				"humidity": 91,
-				"temp_kf": -0.14
-			},
-			"weather": [
-				{
-					"id": 500,
-					"main": "Rain",
-					"description": "light rain",
-					"icon": "10d"
-				}
-			],
-			"clouds": {
-				"all": 100
-			},
-			"wind": {
-				"speed": 4.54,
-				"deg": 216,
-				"gust": 8.52
-			},
-			"visibility": 10000,
-			"pop": 0.66,
-			"rain": {
-				"3h": 0.33
-			},
-			"sys": {
-				"pod": "d"
-			},
-			"dt_txt": "2025-11-19 15:00:00"
-		};
+    dt: 1763564400,
+    main: {
+        temp: 277.84,
+        feels_like: 274.33,
+        temp_min: 277.84,
+        temp_max: 277.98,
+        pressure: 1002,
+        sea_level: 1002,
+        grnd_level: 993,
+        humidity: 91,
+        temp_kf: -0.14,
+    },
+    weather: [
+        {
+            id: 500,
+            main: "Rain",
+            description: "light rain",
+            icon: "10d",
+        },
+    ],
+    clouds: {
+        all: 100,
+    },
+    wind: {
+        speed: 4.54,
+        deg: 216,
+        gust: 8.52,
+    },
+    visibility: 10000,
+    pop: 0.66,
+    rain: {
+        "3h": 0.33,
+    },
+    sys: {
+        pod: "d",
+    },
+    dt_txt: "2025-11-19 15:00:00",
+};
 
-const tomatoHtmlElement = document.getElementById('tomatoes');
+const tomatoHtmlElement = document.getElementById("tomatoes");
 init();
 
-function init(){
-	showTomatoData(exampleData);
+function init() {
+    showTomatoData(exampleData);
 }
 
 function showTomatoData(data) {
-   let rain = data.rain ? data.rain["3h"] : 0;
-   let humidity = data.main.humidity;
 
-   const humidityMeter = document.getElementById('humidity-meter');
-   const humidityText = document.getElementById('humidity-text');
-    humidityMeter.setAttribute('aria-valuenow', humidity);
-    humidityMeter.querySelector('.progress-bar').style.width = `${humidity}%`;
+    let rain = data.rain ? data.rain["3h"] : 0;
+    let humidity = data.main.humidity;
+    const humidityMeter = document.getElementById("humidity-meter");
+    const humidityText = document.getElementById("humidity-text");
+
+    humidityMeter.setAttribute("aria-valuenow", humidity);
+    humidityMeter.querySelector(".progress-bar").style.width = `${humidity}%`;
     humidityText.innerText = `${humidity}%`;
-    const rainMeter = document.getElementById('rain-meter');
-    const rainText = document.getElementById('rain-text');
-    rainMeter.setAttribute('aria-valuenow', rain);
+	
+    const rainMeter = document.getElementById("rain-meter");
+    const rainText = document.getElementById("rain-text");
+    rainMeter.setAttribute("aria-valuenow", rain);
     rainText.innerText = `${rain} mm`;
-    rainText.style.height = `${rain*40}%`;
+    rainText.style.height = `${rain * 40}%`;
 }
