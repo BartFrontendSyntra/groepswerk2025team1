359707ede3bc41d16216f048df0c91248b9790c4
houssin-99 <houssinsyr@gmail.com>
2025-11-20 09:48:36 +0100

    refactor showVampireData to display sunrise, sunset, and cloud cover information

--- vampires.js
+++ vampires.js
@@ -51,6 +51,29 @@ const otherExampleData = {
 const vampireHtmlElement = document.getElementById('vampires');
 init();
 
-function init(){
-	showVampireData(otherExampleData);
+function init() {
+    showVampireData(otherExampleData);
+}
+
+function showVampireData(data) {
+
+    // --- SUNRISE ---
+    let sunriseTimestamp = data.sys.sunrise;
+    let sunriseDate = new Date(sunriseTimestamp * 1000);
+    let sunriseHours = sunriseDate.getHours().toString().padStart(2, '0');
+    let sunriseMinutes = sunriseDate.getMinutes().toString().padStart(2, '0');
+    let sunrise = `${sunriseHours}:${sunriseMinutes}`;
+
+    // --- SUNSET ---
+    let sunsetTimestamp = data.sys.sunset;
+    let sunsetDate = new Date(sunsetTimestamp * 1000);
+    let sunsetHours = sunsetDate.getHours().toString().padStart(2, '0');
+    let sunsetMinutes = sunsetDate.getMinutes().toString().padStart(2, '0');
+    let sunset = `${sunsetHours}:${sunsetMinutes}`;
+
+    // --- CLOUD COVER ---
+    let cloudCover = data.clouds.all;   // 
+
+    document.getElementById("vampire-sunhours").innerText =
+        `Sunrise: ${sunrise} | Sunset: ${sunset} | Clouds: ${cloudCover}%`;
 }
