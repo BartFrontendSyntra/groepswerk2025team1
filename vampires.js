44ecdacdcba9ea0481e4a7fb89e298ce360c7944
houssin-99 <houssinsyr@gmail.com>
2025-11-20 20:20:20 +0100

    Refactor vampires.js for improved readability; add comments and adjust formatting

--- vampires.js
+++ vampires.js
@@ -1,7 +1,7 @@
 // const { data } = require("framer-motion/client");
 
 const vampireHtmlElement = document.getElementById("vampires");
-
+// show vampire data
 function showVampireData(data) {
     // --- SUNRISE ---
 
@@ -15,7 +15,7 @@ function showVampireData(data) {
     let cloudCover = data.clouds.all;
 
     // Fill in the HTML elements
-    document.getElementById("vampire-sunrise").innerText =
+    document.getElementById("vampire-sunrise").innerText = 
         convertUnixToTime(sunrise);
 
     document.getElementById("vampire-sunset").innerText =
@@ -26,7 +26,7 @@ function showVampireData(data) {
 
 // convert time to hours and minutes
 function convertUnixToTime(timestamp) {
-    const date = new Date(timestamp * 1000); // API gives seconds → JS needs ms
+    const date = new Date(timestamp * 1000); // API gives seconds
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
     return `${hours}:${minutes}`;
